618e35c1da08a437e10734c90ec3673be5d2c9d7
Hedzr Yeh <hedzrz@gmail.com>
2021-02-13 09:19:23 +0800

    imp: 3 builds

---
+++ cmake/package-dmg.cmake
@@ -0,0 +1,22 @@
+set(CPACK_GENERATOR "DragNDrop") # DMG
+
+#Generators
+#7Z                           = 7-Zip file format
+#Bundle                       = Mac OSX bundle
+#DragNDrop                    = Mac OSX Drag And Drop
+#External                     = CPack External packages
+#IFW                          = Qt Installer Framework
+#NSIS                         = Null Soft Installer
+#NSIS64                       = Null Soft Installer (64-bit)
+#NuGet                        = NuGet packages
+#OSXX11                       = Mac OSX X11 bundle
+#PackageMaker                 = Mac OSX Package Maker installer
+#STGZ                         = Self extracting Tar GZip compression
+#TBZ2                         = Tar BZip2 compression
+#TGZ                          = Tar GZip compression
+#TXZ                          = Tar XZ compression
+#TZ                           = Tar Compress compression
+#TZST                         = Tar Zstandard compression
+#ZIP                          = ZIP file format
+#productbuild                 = Mac OSX pkg
+

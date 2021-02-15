751882078e4693511217ebdcf111dce358fdc808
Hedzr Yeh <hedzrz@gmail.com>
2021-02-15 17:09:03 +0800

    bump to v0.2.8

--- cmake/prerequisites.cmake
+++ cmake/prerequisites.cmake
@@ -6,6 +6,7 @@
 set(CMAKE_ALLOW_LOOSE_LOOP_CONSTRUCTS true)
 
 
+# /usr/local/lib/cmake
 # /usr/local/share/cmake/Modules
 # /usr/share/cmake/Modules
 #list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/${CMAKE_SCRIPTS}/Modules:${CMAKE_SOURCE_DIR}/${CMAKE_SCRIPTS}")

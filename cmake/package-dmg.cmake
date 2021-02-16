8417653e74287fd5426359a394306ecb5cae891c
Hedzr Yeh <hedzrz@gmail.com>
2021-02-16 09:31:39 +0800

    updated packing name

--- cmake/package-dmg.cmake
+++ cmake/package-dmg.cmake
@@ -3,11 +3,17 @@ set(CPACK_GENERATOR "DragNDrop") # DMG
 # https://cmake.org/cmake/help/latest/module/CPack.html
 # https://cmake.org/cmake/help/latest/cpack_gen/dmg.html#cpack_gen:CPack%20DragNDrop%20Generator
 
-set(CPACK_RPM_PACKAGE_NAME "cmdr11-devel")
-set(CPACK_RPM_PACKAGE_MAINTAINER ${CPACK_PACKAGE_CONTACT})
-set(CPACK_RPM_PACKAGE_DESCRIPTION ${CPACK_PACKAGE_DESCRIPTION})
+set(CPACK_DMG_PACKAGE_NAME "cmdr11-devel")
+set(CPACK_DMG_PACKAGE_MAINTAINER ${CPACK_PACKAGE_CONTACT})
+set(CPACK_DMG_PACKAGE_DESCRIPTION ${CPACK_PACKAGE_DESCRIPTION})
+
+#message(STATUS "CPACK_DMG_VOLUME_NAME = ${CPACK_DMG_VOLUME_NAME}")
 
 #set(CPACK_RPM_PACKAGE_VERSION ${PROJECT_VERSION})
+
+
+set(CPACK_TARGET_FILE_NAME "${PROJECT_NAME}-${PROJECT_VERSION}-${CMAKE_HOST_SYSTEM_NAME}.dmg")
+
 
 #Generators
 #7Z                           = 7-Zip file format

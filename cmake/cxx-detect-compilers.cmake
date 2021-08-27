01efbc3a0b237c90516b5bf83e54f66ec11f054c
Hedzr Yeh <hedzrz@gmail.com>
2021-08-27 10:44:16 +0800

    remove write_compiler_detection_header

--- cmake/cxx-detect-compilers.cmake
+++ cmake/cxx-detect-compilers.cmake
@@ -1,24 +1,24 @@
 
-# https://cmake.org/cmake/help/latest/module/WriteCompilerDetectionHeader.html
-# include(WriteCompilerDetectionHeader)
-# https://cmake.org/cmake/help/v3.14/manual/cmake-compile-features.7.html#manual:cmake-compile-features(7)
-set(WriterCompilerDetectionHeaderFound NOTFOUND)
-# This module is only available with CMake >=3.1, so check whether it could be found
-# BUT in CMake 3.1 this module doesn't recognize AppleClang as compiler, so just use it as of CMake 3.2
-if (${CMAKE_VERSION} VERSION_GREATER "3.2")
-    include(WriteCompilerDetectionHeader OPTIONAL RESULT_VARIABLE WriterCompilerDetectionHeaderFound)
-endif ()
-if (WriterCompilerDetectionHeaderFound)
-    write_compiler_detection_header(
-            FILE ${CMAKE_GENERATED_DIR}/the-compiler.h
-            PREFIX The
-            COMPILERS GNU Clang MSVC Intel AppleClang     #  SunPro
-            FEATURES
-            cxx_variadic_templates cxx_final cxx_override
-            cxx_static_assert cxx_constexpr cxx_extern_templates
-            cxx_noexcept cxx_thread_local
-    )
-endif ()
+## https://cmake.org/cmake/help/latest/module/WriteCompilerDetectionHeader.html
+## include(WriteCompilerDetectionHeader)
+## https://cmake.org/cmake/help/v3.14/manual/cmake-compile-features.7.html#manual:cmake-compile-features(7)
+#set(WriterCompilerDetectionHeaderFound NOTFOUND)
+## This module is only available with CMake >=3.1, so check whether it could be found
+## BUT in CMake 3.1 this module doesn't recognize AppleClang as compiler, so just use it as of CMake 3.2
+#if (${CMAKE_VERSION} VERSION_GREATER "3.2")
+#    include(WriteCompilerDetectionHeader OPTIONAL RESULT_VARIABLE WriterCompilerDetectionHeaderFound)
+#endif ()
+#if (WriterCompilerDetectionHeaderFound)
+#    write_compiler_detection_header(
+#            FILE ${CMAKE_GENERATED_DIR}/the-compiler.h
+#            PREFIX The
+#            COMPILERS GNU Clang MSVC Intel AppleClang     #  SunPro
+#            FEATURES
+#            cxx_variadic_templates cxx_final cxx_override
+#            cxx_static_assert cxx_constexpr cxx_extern_templates
+#            cxx_noexcept cxx_thread_local
+#    )
+#endif ()
 
 #
 # CMAKE_POSITION_INDEPENDENT_CODE

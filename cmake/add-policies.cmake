2a2a0016aca9d8b8b7f2820b264461afc2320618
Hedzr Yeh <hedzrz@gmail.com>
2021-08-28 08:43:18 +0800

    more compatibilities on cmake policies

--- cmake/add-policies.cmake
+++ cmake/add-policies.cmake
@@ -17,14 +17,32 @@ if (COMMAND cmake_policy)
 endif ()
 
 
-cmake_policy(SET CMP0042 NEW) # ENABLE CMP0042: MACOSX_RPATH is enabled by default.
-cmake_policy(SET CMP0048 NEW)
-cmake_policy(SET CMP0054 NEW) # ENABLE CMP0054: Only interpret if() arguments as variables or keywords when unquoted.
-cmake_policy(SET CMP0063 NEW) # ENABLE CMP0063: Honor visibility properties for all target types.
-cmake_policy(SET CMP0069 NEW)
+if (POLICY CMP0042)
+    cmake_policy(SET CMP0042 NEW) # ENABLE CMP0042: MACOSX_RPATH is enabled by default.
+endif ()
+if (POLICY CMP0048)
+    cmake_policy(SET CMP0048 NEW)
+endif ()
+
+if (POLICY CMP0054)
+    cmake_policy(SET CMP0054 NEW) # ENABLE CMP0054: Only interpret if() arguments as variables or keywords when unquoted.
+endif ()
+if (POLICY CMP0063)
+    cmake_policy(SET CMP0063 NEW) # ENABLE CMP0063: Honor visibility properties for all target types.
+endif ()
+if (POLICY CMP0069)
+    cmake_policy(SET CMP0069 NEW)
+endif ()
+#if (POLICY CMP0070)
 #cmake_policy(SET CMP0070 NEW)
-cmake_policy(SET CMP0076 NEW) # target_sources() command converts relative paths to absolute
-cmake_policy(SET CMP0077 NEW) # ENABLE CMP0077: option() honors normal variables
+#endif ()
+if (POLICY CMP0076)
+    cmake_policy(SET CMP0076 NEW) # target_sources() command converts relative paths to absolute
+endif ()
+
+if (POLICY CMP0077)
+    cmake_policy(SET CMP0077 NEW) # ENABLE CMP0077: option() honors normal variables
+endif ()
 
 if (POLICY CMP0120)
     cmake_policy(SET CMP0120 NEW) # The WriteCompilerDetectionHeader module is removed

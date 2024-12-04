b301672e5557219bce006371c03910492b88f838
hz <hedzrz@gmail.com>
2024-12-04 08:06:21 +0800

    improved test_types_check_1

--- libs/cmdr11/tests/main.cxx
+++ libs/cmdr11/tests/main.cxx
@@ -10,6 +10,7 @@
 #include <string>
 #include <vector>
 
+#include "cmdr11/cmdr_if.hh"
 #include "cmdr11/cmdr_var_t.hh"
 
 #if JUST_FOR_REFERRING
@@ -31,27 +32,27 @@ int main(int, char **) {
 void test_types_check_1() {
   std::cout << std::boolalpha;
 
-  std::cout << "is_vector_t: \n";
-  std::cout << cmdr::is_vector_v<std::vector<int>> << std::endl;
-  std::cout << cmdr::is_vector_v<int> << std::endl;
+  std::cout << "cmdr::traits::is_vector_t: \n";
+  std::cout << std::boolalpha << cmdr::traits::is_vector_v<std::vector<int>> << std::endl;
+  std::cout << std::boolalpha << cmdr::traits::is_vector_v<int> << std::endl;
 
-  std::cout << "is_container: \n";
-  std::cout << cmdr::is_container<std::string>::value << std::endl;
-  std::cout << cmdr::is_container<std::list<std::string>>::value << std::endl;
+  std::cout << "cmdr::traits::is_container: \n";
+  std::cout << std::boolalpha << cmdr::traits::is_container<std::string>::value << std::endl;
+  std::cout << std::boolalpha << cmdr::traits::is_container<std::list<std::string>>::value << std::endl;
 
   using namespace std::chrono_literals;
 
   auto v1 = std::vector{"example.com", "example.org"};
-  std::cout << "cmdr::is_stl_container : " << cmdr::is_stl_container<decltype(v1)>::value << std::endl;
+  std::cout << "cmdr::traits::is_stl_container : " << cmdr::traits::is_stl_container<decltype(v1)>::value << std::endl;
   auto v2 = 10s;
-  std::cout << "type " << typeid(v2).name() << ". check: " << cmdr::is_duration<decltype(v2)>::value << std::endl;
+  std::cout << "type " << typeid(v2).name() << ". check: " << cmdr::traits::is_duration<decltype(v2)>::value << std::endl;
 
-  std::cout << "cmdr::is_duration<std::chrono::seconds> : " << cmdr::is_duration<std::chrono::seconds>::value << std::endl;
-  std::cout << "cmdr::is_duration<std::chrono::hours> : " << cmdr::is_duration<std::chrono::hours>::value << std::endl;
-  std::cout << "cmdr::is_duration<std::chrono::microseconds> : " << cmdr::is_duration<std::chrono::microseconds>::value << std::endl;
-  std::cout << "cmdr::is_duration<std::chrono::nanoseconds> : " << cmdr::is_duration<std::chrono::nanoseconds>::value << std::endl;
-  std::cout << "cmdr::is_duration<long long> : " << cmdr::is_duration<decltype(0xfull)>::value << std::endl;
-  std::cout << "cmdr::is_duration<bool> : " << cmdr::is_duration<bool>::value << std::endl;
+  std::cout << "cmdr::traits::is_duration<std::chrono::seconds> : " << cmdr::traits::is_duration<std::chrono::seconds>::value << std::endl;
+  std::cout << "cmdr::traits::is_duration<std::chrono::hours> : " << cmdr::traits::is_duration<std::chrono::hours>::value << std::endl;
+  std::cout << "cmdr::traits::is_duration<std::chrono::microseconds> : " << cmdr::traits::is_duration<std::chrono::microseconds>::value << std::endl;
+  std::cout << "cmdr::traits::is_duration<std::chrono::nanoseconds> : " << cmdr::traits::is_duration<std::chrono::nanoseconds>::value << std::endl;
+  std::cout << "cmdr::traits::is_duration<long long> : " << cmdr::traits::is_duration<decltype(0xfull)>::value << std::endl;
+  std::cout << "cmdr::traits::is_duration<bool> : " << cmdr::traits::is_duration<bool>::value << std::endl;
 }
 
 void test_var_t_1() {

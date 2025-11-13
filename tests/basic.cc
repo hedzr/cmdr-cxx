bf9c9dbd37519abd10cf29aa8f9fccc8cf047215
hz <hedzrz@gmail.com>
2025-11-13 08:29:26 +0800

    improved basic.cc, make it as a hello-world app skeleton

--- tests/basic.cc
+++ tests/basic.cc
@@ -8,10 +8,36 @@
 // Created by Hedzr Yeh on 2021/10/25.
 //
 
+#if (__cplusplus > 202002L) // c++23 and higher
+
+// import std;  // not yet
+
+#include <cstdlib> // for EXIT_FAILURE, ...
+#include <print>   // this form should work down to c++17+
+// #include <iomanip>
+// #include <iostream>
+
+#include "cmdr-cxx.hh"
+
+auto main(int argc, char *argv[]) -> int {
+  cmdr::test::test_for_macros(); // internal tests
+
+  // std::println("Hello, World!\n");
+  std::print("Hello, {}! __cplusplus = {}.\n", "World", __cplusplus); // outputs: Hello, World!
+  std::print("The answer is: {}\n", 42);                              // outputs: The answer is: 42
+
+  if (argc < 1) {
+    fprintf(stderr, "err: we need any arguments passed in.\n");
+    return EXIT_FAILURE; // = 1
+  }
+
+  UNUSED(argv);
+  return EXIT_SUCCESS; // = 0
+}
+
+#else // below c++23
 #include "cmdr-cxx.hh"
 #include <iomanip>
 #include <iostream>
-
-int main() {
-  cmdr::test::test_for_macros();
-}
+auto main() -> int { std::cout << "Hello, World! " << __cplusplus << '\n'; }
+#endif
